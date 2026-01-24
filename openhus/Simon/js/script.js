5d18d7ace9d732deb5a6dc0ff840958d49f97204
BatughanPek <U2522013@rts-365.dk>
2026-01-24 11:05:43 +0100

    simon

---
+++ openhus/Simon/js/script.js
@@ -0,0 +1,13 @@
+let buttonColors = ['red', 'blue', 'green', 'yellow'];
+let gamePattern = [];
+
+
+function nextSequence() {
+    userClickedPattern = [];
+    let randomNumber = math.floor(math.random() *4);
+    let randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+    function fadeOut(randomChosenColor){
+
+    }
+}
