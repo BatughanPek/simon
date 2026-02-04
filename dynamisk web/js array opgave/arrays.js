aa6cd89e9d82522af2cf59bda0a90d542d1ed730
BatughanPek <U2522013@rts-365.dk>
2026-02-04 13:37:07 +0100

    array

--- dynamisk web/js array opgave/arrays.js
+++ dynamisk web/js array opgave/arrays.js
@@ -7,7 +7,26 @@ let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
 
 
 
-// --- opgave: find alle tal over 20 ---
+// --- opgave 1 ---
 let newNumbers = numbersArray.filter(num => num > 20);
-console.log(newNumbers);
+console.log('opgave 1', newNumbers);
 
+// --- opgave 2 ---
+catsArray.push("Sniffles");
+console.log('opgave 2', catsArray);
+
+// --- opgave 3 ---
+let hasTrixie = dogsArray.includes("Trixie");
+console.log('opgave 3a', hasTrixie);
+let hasBaxter = dogsArray.includes("Baxter");
+console.log('opgave 3b', hasBaxter);
+
+// --- opgave 4 ---
+let foundCat = catsArray.find(cat => cat === "Bagheera");
+console.log('opgave 4a', foundCat);
+let foundSalem = catsArray.find(cat => cat === "Salem");
+console.log('opgave 4b', foundSalem);
+
+// --- opgave 5 ---
+let numbersTimesThree = numbersArray.map(num => num * 3);
+console.log('opgave 5', numbersTimesThree);
